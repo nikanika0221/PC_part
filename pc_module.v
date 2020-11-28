3a4c68e5adfd7ef8d4d49176eff73e58b1ee85fa
nikanika0221 <60596671+nikanika0221@users.noreply.github.com>
2020-11-28 17:17:26 +0400

    Add files via upload

---
+++ pc_module.v
@@ -0,0 +1,21 @@
+`timescale 1ns / 1ps
+
+module pc_module(
+		input clk,
+		input sel,
+		input[7:0] jump_to,
+		output[7:0] R15_value,
+		output reg[7:0] PC_out
+    );
+	
+	wire[7:0] mux_out;
+	
+
+	always @(posedge clk) begin
+		PC_out <= mux_out;
+	end
+
+	assign mux_out = sel ? jump_to : PC_out+1; 
+	assign R15_value = PC_out+2;
+	
+endmodule
---
+++ tb.v
@@ -0,0 +1,31 @@
+
+module tb;
+
+	// Inputs
+	reg clk;
+	reg sel;
+	reg [7:0] jump_to;
+
+	// Outputs
+	wire [7:0] R15_value;
+	wire [7:0] PC_out;
+
+	// Instantiate the Unit Under Test (UUT)
+	pc_module uut (
+		.clk(clk), 
+		.sel(sel), 
+		.jump_to(jump_to), 
+		.R15_value(R15_value), 
+		.PC_out(PC_out)
+	);
+	always #5 clk <= !clk;
+	always #50 sel <= !sel;
+	always #50 jump_to <= jump_to + 5;
+	initial begin
+		// Initialize Inputs
+		clk = 0;
+		sel =0;
+		jump_to = 0;
+	end 
+endmodule
+
